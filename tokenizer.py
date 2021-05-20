603e725df54402608d035b64013e9bb23dbf92a7
jitesh1059 <jiteshmoganaraja@gmail.com>
2021-05-20 15:21:53 +0000

    Final Update

--- tokenizer.py
+++ tokenizer.py
@@ -1,6 +1,34 @@
 from curtsies.fmtfuncs import red, bold, green, on_blue, yellow, blue, cyan
 from tokenizers import ByteLevelBPETokenizer
+from transformers import GPT2Config, GPT2LMHeadModel, GPT2Tokenizer
 
+TRAIN_BASE = False
 paths = ["python_code_data.txt"]
 
-tokenizer = ByteBevelBPETokenizer
+if TRAIN_BASE:
+    tokenizer = ByteLevelBPETokenizer()
+
+    tokenizer.train(files=paths, vocab_size=52_000, min_frequency=2, special_tokens=[
+        "<s>",
+        "<pad>",
+        "</s>",
+        "<unk>",
+        "<mask>",
+    ])
+
+    # Save files to disk
+    tokenizer.save_model("tokenizer")
+
+inp = "print('Hello World')"
+tokenizer = GPT2Tokenizer.from_pretrained("tokenizer")
+
+tokenizer.add_special_tokens({
+    "eos_token": "</s>",
+    "bos_token": "<s>",
+    "unk_token": "<unk>",
+    "pad_token": "<pad>",
+    "mask_token": "<mask>"
+})
+
+t = tokenizer.encode(inp)
+print(t)
