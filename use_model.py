c0abf861bf2d6e05ebc01640f86d6483c49ba14e
Jitesh <jiteshmoganaraja@gmail.com>
2021-05-25 11:43:17 +0800

    Final Push

--- use_model.py
+++ use_model.py
@@ -37,40 +37,21 @@ print(t)
 
 print(tokenizer.decode(t))
 
-config = GPT2Config(
-    vocab_size = tokenizer.vocab_size,
-    bos_token = tokenizer.bos_token_id,
-    eos_token = tokenizer.eos_token_id
+model = GPT2LMHeadModel.from_pretrained("GPyT").to("cuda")
+
+while True:
+    inp = input(">>> ")
+    input_ids = tokenizer.encode(inp, return_tensors="pt").to("cuda")
+    beam_output= model.generate(
+        input_ids,
+        max_length = 512,
+        num_beams = 10,
+        temperature = 0.7,
+        no_repeat_ngram_size = 5,
+        num_return_sequences = 1
     )
+    for beam in beam_output:
+        out = tokenizer.decode(beam)
+        fout = out.replace("<N>", "\n")
 
-model = GPT2LMHeadModel(config)
-
-dataset = load_dataset("text", data_files=paths)
-
-def encode(lines):
-    return tokenizer(lines["text"], add_special_tokens=True, truncation=True, max_length=512)
-
-dataset.set_transform(encode)
-dataset = dataset["train"]
-
-data_collator = DataCollatorForLanguageModelling(tokenizer=tokenizer, mlm=True, mlm_probability=0.15)
-
-training_args = TrainingArguments(
-    output_dir = "GpYT",
-    overwrite_output_dir = True,
-    num_train_epochs = 1,
-    per_device_train_batch_size = 10,
-    save_steps = 100,
-    save_total_limit = 2,
-    prediction_loss_only = True,
-)
-
-trainer = Trainer(
-    model = model,
-    args=training_args,
-    data_collator = data_collator,
-    train_dataset=dataset,
-)
-
-trainer.train()
-trainer.save_model("GPyT")
+        print(green(str(fout)))
